9f352c633d6d042d461f6ffff90764cd4e7c3c57
NurulAis <nurul.byn10@gmail.com>
2024-11-10 19:17:30 +0700

    upadete file main.py

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ scaler.fit(dataset_nutrisi)
 dataset_nutrisi_scaled = scaler.fit_transform(dataset_nutrisi)
 
 application = FastAPI()
-#model = load_model('model_rekomedasi.h5')
+model = load_model('./model_rekomendasi.h5')
 
-@application.get('/recommendation')
+
+@application.post('/recommendation')
 def food_recommendation(cal:int, fat:int,sat:int, chol:int, sodium:int, carbo:int, fiber:int, sugar:int, pro:int):
     input_nutrisi = np.array([[cal, fat, sat, chol, sodium, carbo, fiber, sugar, pro]])
     input_nutrisi_scaled = scaler.transform(input_nutrisi)
-    similarity = cosine_similarity(input_nutrisi_scaled, dataset_nutrisi_scaled)
+    predicted_latent_feature = model.predict(dataset_nutrisi_scaled)
+    similarity = cosine_similarity(input_nutrisi_scaled, predicted_latent_feature)
     top_resep = np.argsort(similarity, axis=1)[0][::-1][10]
     kolom_pilihan = ['Name', 'Images', 'RecipeIngredientParts',
                  'Calories', 'FatContent', 'SaturatedFatContent', 'CholesterolContent',
